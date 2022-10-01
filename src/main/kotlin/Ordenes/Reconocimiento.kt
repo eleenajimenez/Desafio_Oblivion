2e2e12d84855cc6b50a18b684eab5293b1122dff
Elena <elenagomezlimon@gmail.com>
2022-10-01 11:38:34 +0200

    Builders de las clases heredadas de orden

--- src/main/kotlin/Ordenes/Reconocimiento.kt
+++ src/main/kotlin/Ordenes/Reconocimiento.kt
@@ -1,4 +1,25 @@
 package Ordenes
 
-class Reconocimiento {
+import java.util.*
+
+class Reconocimiento(fecha:Date?= null, completado:Boolean?= null, cuadrante_dest:Array<Any?>?= null, var area_conocer:Int?=null, var ha_encontrado:String?=null): Orden(fecha, completado, cuadrante_dest) {
+
+    class Builder(var fecha:Date?= null, var completado:Boolean?= null, var cuadrante_dest:Array<Any?>?= null, var area_conocer:Int?=null, var ha_encontrado:String?=null){
+        fun area_conocer(area_conocer: Int?): Builder {
+            this.area_conocer= area_conocer
+            return this
+        }
+        fun ha_encontrado(ha_encontrado: String?): Builder {
+            this.ha_encontrado= ha_encontrado
+            return this
+        }
+
+        fun build():Reconocimiento{
+            return Reconocimiento(fecha, completado, cuadrante_dest, area_conocer, ha_encontrado)
+        }
+    }
+
+    override fun toString(): String {
+        return "Reconocimiento: (${super.toString()}, Área a conocer: " + this.area_conocer + ", Que ha encontrado: " + this.ha_encontrado
+    }
 }
--- src/main/kotlin/Ordenes/Reparacion.kt
+++ src/main/kotlin/Ordenes/Reparacion.kt
@@ -1,4 +1,33 @@
 package Ordenes
 
-class Reparacion {
+import java.util.*
+
+class Reparacion(fecha: Date?= null, completado:Boolean?= null, cuadrante_dest:Array<Any?>?= null, var id_dron:Int?=null, var estado:Boolean?=null): Orden(fecha, completado, cuadrante_dest) {
+
+    class Builder(
+        var fecha: Date? = null,
+        var completado: Boolean? = null,
+        var cuadrante_dest: Array<Any?>? = null,
+        var id_dron: Int? = null,
+        var estado: Boolean? = null
+    ) {
+        fun id_dron(id_dron: Int?): Builder {
+            this.id_dron = id_dron
+            return this
+        }
+
+        fun estado(estado: Boolean?): Builder {
+            this.estado = estado
+            return this
+        }
+
+        fun build(): Reparacion {
+            return Reparacion(fecha, completado, cuadrante_dest, id_dron, estado)
+        }
+    }
+
+    override fun toString(): String {
+        return "Reparacion: (${super.toString()}, id_dron:  " + this.id_dron + ", estado: " + this.estado
+    }
+
 }
